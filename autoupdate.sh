6310c95b16ed899cacd7451e8db28ea9e74aee3b
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 22:42:15 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -38,13 +38,13 @@ echo 'LATEST_TAG=$(git tag --sort=-creatordate | head -1)' >> $SCRIPT_NAME
 echo 'cd ..' >> $SCRIPT_NAME
 echo -n 'if [[ -z $' >> $SCRIPT_NAME
 echo -e "($HOMEFOLDER/nknd -v | grep $LATEST_TAG) ]]; then" >> $SCRIPT_NAME
-echo -e "  sudo systemctl stop nkn.service" >> $SCRIPT_NAME
+echo -e "  sudo -u $USER systemctl stop nkn.service" >> $SCRIPT_NAME
 echo '  if [ -f $DIR_NAME.zip ]; then rm $DIR_NAME.zip; fi' >> $SCRIPT_NAME
 echo '  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"' >> $SCRIPT_NAME
 echo '  unzip "$DIR_NAME.zip" >/dev/null 2>&1' >> $SCRIPT_NAME
 echo '  chmod +x $DIR_NAME/nkn*' >> $SCRIPT_NAME
 echo '  mv $DIR_NAME/nkn* .' >> $SCRIPT_NAME
-echo -e "  sudo systemctl start nkn.service" >> $SCRIPT_NAME
+echo -e "  sudo -u $USER systemctl start nkn.service" >> $SCRIPT_NAME
 echo '  rm -rf $DIR_NAME' >> $SCRIPT_NAME
 echo '  rm $DIR_NAME.zip' >> $SCRIPT_NAME
 echo 'fi' >> $SCRIPT_NAME
