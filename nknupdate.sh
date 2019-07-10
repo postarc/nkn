194a0787d6a399c52a810041aa52d7adf2d859b3
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 15:43:09 +0300

    Create nknupdate.sh

---
+++ nknupdate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+GITPATH="https://github.com/nknorg/nkn.git"
+RELEASES_PATH="https://github.com/nknorg/nkn/releases/download"
+DIR_NAME="linux-amd64"
+
+
+if [[ "$USER" == "root" ]]; then
+        HOMEFOLDER="/root/nkn-node"
+ else
+        HOMEFOLDER="/home/$USER/nkn-node"
+fi 
+
+cd $HOMEFOLDER
+if [ -d nkn ]; then 
+  cd nkn
+  git merge
+  else
+  git clone $GITPATH
+  cd nkn
+fi
+LATEST_TAG=$(git tag --sort=-creatordate | head -1)
+cd ..
+
+if [ ./nknd -v | grep $LATEST_TAG ]; then exit
+else
+  systemctl stop nkn.service
+  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"
+  unzip "$DIR_NAME.zip"
+  chmod +x $DIR_NAME/nkn*
+  mv $DIR_NAME/nkn* .
+  systemctl start nkn.service
+  rm -rf $DIR_NAME $DIR_NAME.zip
+fi  
+cd $CURRENTDIR
