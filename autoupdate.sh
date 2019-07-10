1afb4e0cb8134605ac16ef567bededbcd715bc75
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 15:53:50 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-GITPATH="https://github.com/nknorg/nkn.git"
-ARCHIVE="https://github.com/nknorg/nkn/releases/download/v1.0.2-beta/linux-amd64.zip"
-FNAME="linux-amd64.zip"
-APATH="linux-amd64"
-FCONFIG="config.json"
-FWALLET="wallet.json"
-
 if [[ "$USER" == "root" ]]; then
         HOMEFOLDER="/root/nkn-node"
  else
         HOMEFOLDER="/home/$USER/nkn-node"
 fi 
 
-chmod +x nknupdate.sh
+CURRENTDIR=$(pwd)
+chmod +x nkn/nknupdate.sh
 cp nkn/nknupdate.sh $HOMEFOLDER/
 
 if [ crontab -l | grep "$HOMEFOLDER/nknupdate.sh" ]; then exit; else
@@ -21,17 +15,6 @@ if [ crontab -l | grep "$HOMEFOLDER/nknupdate.sh" ]; then exit; else
         echo -e "00 12 * * * $HOMEFOLDER/nknupdate.sh >/dev/null 2>&1" >> cron
         crontab cron
 fi
-
-
-sudo systemctl stop nkn.service
-cd $HOMEFOLDER
-wget $ARCHIVE
-unzip $FNAME >/dev/null 2>&1
-mv $APATH/nkn* .
-rm -rf $APATH
-rm $FNAME
-
-sudo systemctl start nkn.service
 
 cd $CURRENTDIR
 rm -rf nkn
