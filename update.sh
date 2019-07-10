43bc94f525639198617a2f1bb87d9eda1869aea3
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 12:54:03 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ARCHIVE="https://github.com/nknorg/nkn/releases/download/v1.0.2b-beta/linux-amd64.zip"
+FNAME="linux-amd64.zip"
+APATH="linux-amd64"
+FCONFIG="config.json"
+FWALLET="wallet.json"
+
+if [[ "$USER" == "root" ]]; then
+        HOMEFOLDER="/root/nkn-node"
+ else
+        HOMEFOLDER="/home/$USER/nkn-node"
+fi 
+
+sudo systemctl stop nkn.service
+cd $HOMEFOLDER
+wget $ARCHIVE
+unzip $FNAME >/dev/null 2>&1
+mv $APATH/nkn* .
+rm -rf $APATH
+rm $FNAME
+
+cd $CURRENTDIR
+rm -rf nkn
