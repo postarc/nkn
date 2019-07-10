a2750b88be8d82503eba96bc0c389a2dc0e84879
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 21:39:07 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -18,46 +18,15 @@ if [[ -z $(sudo -u root crontab -l | grep "nknupdate.sh") ]]; then
         rm cron
 fi
 
-cat << EOF > $SCRIPT_NAME
-#!/bin/bash
-
-GITPATH="https://github.com/nknorg/nkn.git"
-RELEASES_PATH="https://github.com/nknorg/nkn/releases/download"
-DIR_NAME="linux-amd64"
-CURRENTDIR=$(pwd)
-EOF
-
+echo "#!/bin/bash\nGITPATH="https://github.com/nknorg/nkn.git"\nRELEASES_PATH="https://github.com/nknorg/nkn/releases/download"\nDIR_NAME="linux-amd64"\nCURRENTDIR=$(pwd)" > $SCRIPT_NAME
 echo -e "cd $HOMEFOLDER" >> $SCRIPT_NAME
 
-cat << EOF >> $SCRIPT_NAME
-if [ -d nkn ]; then
-  cd nkn
-  git merge
-  else
-  git clone $GITPATH
-  cd nkn
-fi
-LATEST_TAG=$(git tag --sort=-creatordate | head -1)
-cd ..
-EOF
-
+echo "if [ -d nkn ]; then\n  cd nkn\n  git merge\n  else\n  git clone $GITPATH\n  cd nkn\nfi\nLATEST_TAG=$(git tag --sort=-creatordate | head -1)\ncd .." >> $SCRIPT_NAME
 echo -e "if [[ -z $($HOMEFOLDER/nknd -v | grep $LATEST_TAG) ]]; then" >> $SCRIPT_NAME
 echo -e "  sudo -u $USER systemctl stop nkn.service" >> $SCRIPT_NAME
-
-cat << EOF >> $SCRIPT_NAME  
-  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"
-  unzip "$DIR_NAME.zip"
-  chmod +x $DIR_NAME/nkn*
-  mv $DIR_NAME/nkn* .
-EOF
-
+echo "  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"\n  unzip "$DIR_NAME.zip"\n  chmod +x $DIR_NAME/nkn*\n  mv $DIR_NAME/nkn* ." >> $SCRIPT_NAME
 echo -e "  sudo -u nkn systemctl start nkn.service" >> $SCRIPT_NAME
-
-cat << EOF >> $SCRIPT_NAME 
-  rm -rf $DIR_NAME $DIR_NAME.zip
-fi
-cd $CURRENTDIR
-EOF
+echo "  rm -rf $DIR_NAME $DIR_NAME.zip\nfi\ncd $CURRENTDIR" >> $SCRIPT_NAME
 
 cd $CURRENTDIR
 rm -rf nkn
