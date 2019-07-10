eccbac2d502804462f17a5e8a99174a916a53b4e
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 16:14:59 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -10,7 +10,7 @@ CURRENTDIR=$(pwd)
 chmod +x nkn/nknupdate.sh
 cp nkn/nknupdate.sh $HOMEFOLDER/
 
-if [ crontab -l | grep "$HOMEFOLDER/nknupdate.sh" ]; then exit; else
+if [[ -z $(crontab -l | grep '/nknupdate.sh') ]]; then
         crontab -l > cron
         echo -e "00 12 * * * $HOMEFOLDER/nknupdate.sh >/dev/null 2>&1" >> cron
         crontab cron
