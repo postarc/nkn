c0d06432779dbc004dbc3f98f06c29e8894f96b2
postarc <45208608+postarc@users.noreply.github.com>
2019-07-10 21:47:18 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -18,15 +18,31 @@ if [[ -z $(sudo -u root crontab -l | grep "nknupdate.sh") ]]; then
         rm cron
 fi
 
-echo "#!/bin/bash\nGITPATH="https://github.com/nknorg/nkn.git"\nRELEASES_PATH="https://github.com/nknorg/nkn/releases/download"\nDIR_NAME="linux-amd64"\nCURRENTDIR=$(pwd)" > $SCRIPT_NAME
+echo "#!/bin/bash" > $SCRIPT_NAME
+echo 'GITPATH="https://github.com/nknorg/nkn.git"' >> $SCRIPT_NAME
+echo 'RELEASES_PATH="https://github.com/nknorg/nkn/releases/download"' >> $SCRIPT_NAME
+echo 'DIR_NAME="linux-amd64"\nCURRENTDIR=$(pwd)"' >> $SCRIPT_NAME
 echo -e "cd $HOMEFOLDER" >> $SCRIPT_NAME
-
-echo "if [ -d nkn ]; then\n  cd nkn\n  git merge\n  else\n  git clone $GITPATH\n  cd nkn\nfi\nLATEST_TAG=$(git tag --sort=-creatordate | head -1)\ncd .." >> $SCRIPT_NAME
+echo 'if [ -d nkn ]; then' >> $SCRIPT_NAME
+echo '  cd nkn' >> $SCRIPT_NAME
+echo '  git merge' >> $SCRIPT_NAME
+echo '  else' >> $SCRIPT_NAME
+echo '  git clone $GITPATH' >> $SCRIPT_NAME
+echo '  cd nkn' >> $SCRIPT_NAME
+echo 'fi' >> $SCRIPT_NAME
+echo 'LATEST_TAG=$(git tag --sort=-creatordate | head -1)' >> $SCRIPT_NAME
+echo 'cd ..' >> $SCRIPT_NAME
 echo -e "if [[ -z $($HOMEFOLDER/nknd -v | grep $LATEST_TAG) ]]; then" >> $SCRIPT_NAME
 echo -e "  sudo -u $USER systemctl stop nkn.service" >> $SCRIPT_NAME
-echo "  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"\n  unzip "$DIR_NAME.zip"\n  chmod +x $DIR_NAME/nkn*\n  mv $DIR_NAME/nkn* ." >> $SCRIPT_NAME
+echo '  wget "$RELEASES_PATH/$LATEST_TAG/$DIR_NAME.zip"' >> $SCRIPT_NAME
+echo '  unzip "$DIR_NAME.zip"' >> $SCRIPT_NAME
+echo '  chmod +x $DIR_NAME/nkn*' >> $SCRIPT_NAME
+echo '  mv $DIR_NAME/nkn* .' >> $SCRIPT_NAME
 echo -e "  sudo -u nkn systemctl start nkn.service" >> $SCRIPT_NAME
-echo "  rm -rf $DIR_NAME $DIR_NAME.zip\nfi\ncd $CURRENTDIR" >> $SCRIPT_NAME
+echo '  rm -rf $DIR_NAME $DIR_NAME.zip' >> $SCRIPT_NAME
+echo 'fi' >> $SCRIPT_NAME
+echo 'cd $CURRENTDIR' >> $SCRIPT_NAME
 
+chmod +x $SCRIPT_NAME
 cd $CURRENTDIR
 rm -rf nkn
