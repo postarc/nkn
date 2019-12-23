61abeec950a6ffbc42e886d947b19b18890ac3ab
postarc <45208608+postarc@users.noreply.github.com>
2019-12-23 22:17:29 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ cp -r linux-amd64/nkn* nkn-node
 cp -r linux-amd64/certs nkn-node
 cp -r linux-amd64/web nkn-node
 rm -rf linux-amd64 linux-amd64.zip nkn
-
+rm -rf nkn-node/Log
 sudo systemctl start nkn.service
