a5366f082f55234740f32a82cdc6e961a64fc062
postarc <45208608+postarc@users.noreply.github.com>
2019-12-23 22:07:59 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,9 @@
 sudo systemctl stop nkn.service
 wget https://github.com/nknorg/nkn/releases/download/v1.1.2-beta.1/linux-amd64.zip
 unzip linux-amd64.zip >/dev/null 2>&1
-mv linux-amd64/nkn* nkn-node/.
-mv linux-amd64/certs nkn-node/.
-mv linux-amd64/web nkn-node/.
+cp -r linux-amd64/nkn* nkn-node
+cp -r linux-amd64/certs nkn-node
+cp -r linux-amd64/web nkn-node
 rm -rf linux-amd64 linux-amd64.zip nkn
 
 sudo systemctl start nkn.service
