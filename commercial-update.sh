0d461b02fdb3ef80f2041bfbf36b13831ffa86a9
postarc <45208608+postarc@users.noreply.github.com>
2020-07-02 18:18:55 +0300

    Update commercial-update.sh

--- commercial-update.sh
+++ commercial-update.sh
@@ -58,8 +58,8 @@ if [ -f $SYSTEMD_PATH/$SERVICE_NAME ]; then
         sed -i "s/.*WorkingDirectory.*/$HOMEDIR/" $SYSTEMD_PATH/$SERVICE_NAME
         sed -i 's/.*RestartSec.*/RestartSec=1/' $SYSTEMD_PATH/$SERVICE_NAME
         sed -i 's/.*Description.*/Description=nkn-commercial/' $SYSTEMD_PATH/$SERVICE_NAME
-        if ! cat $SYSTEMD_PATH/$SERVICE_NAME | grep "After="; then sed -i '/^Description/a\After=network-online.target' $SYSTEMD_PATH/$SERVICE_NAME; fi >/dev/null 2>&1
-        if ! cat $SYSTEMD_PATH/$SERVICE_NAME | grep "Type=simple"; then sed -i '/^Service/a\Type=simple' $SYSTEMD_PATH/$SERVICE_NAME; fi >/dev/null 2>&1
+        if ! cat $SYSTEMD_PATH/$SERVICE_NAME | grep "After="; then sed -i '/^Description/a\After=network-online.target' $SYSTEMD_PATH/$SERVICE_NAME; fi 
+        if ! cat $SYSTEMD_PATH/$SERVICE_NAME | grep "Type=simple"; then sed -i '/^Service/a\Type=simple' $SYSTEMD_PATH/$SERVICE_NAME; fi 
         sed -i 's/.*WantedBy.*/WantedBy=multi-user.target/' $SYSTEMD_PATH/$SERVICE_NAME
         sudo systemctl daemon-reload
 else
