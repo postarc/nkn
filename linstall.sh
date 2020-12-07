b9029414435c3e6299d9fe065b07caa77d3a9fb9
postarc <45208608+postarc@users.noreply.github.com>
2020-12-07 16:40:26 +0300

    Update linstall.sh

--- linstall.sh
+++ linstall.sh
@@ -12,6 +12,9 @@ MAG='\e[1;35m'
 
 SERVER_NAME="nkn-node"
 STARTING_COUNTER=1
+LOG_FILE='linstall.log'
+#SERVER_IP_FILE='serverip.list'
+
 
 echo -e -n "${YELLOW}Personal Access Token: ${NC}"
 read TOKEN
@@ -35,27 +38,42 @@ sudo apt update
 sudo apt -y install curl jq
 
 echo -e "${CYAN}Stackscripts creating...${NC}"
-if [ -f stackscripts.id ]; then
-	STACK_ID=$(cat stackscripts.id)
-else
-    JSON_DATA="{ \"label\" : \"$SERVER_NAME\","
-    JSON_DATA+='"description" : "NKN node Installing","images" : [ "linode/ubuntu16.04lts","linode/ubuntu18.04","linode/ubuntu19.10","linode/ubuntu20.04" ],'
-    JSON_DATA+='"is_public" : false,"rev_note" : "Set up NKN node","script" : "#!/bin/bash\ngit clone https://github.com/postarc/nkn.git\nsudo bash nkn/installmc.sh\nsudo systemctl restart nkn.service\n" }'
-    #echo $JSON_DATA | jq '.'
-    curl -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" \
-       -X POST -d "${JSON_DATA}"  https://api.linode.com/v4/linode/stackscripts | jq '.id | tonumber' >> stackscripts.id
-fi
+JSON_DATA="{ \"label\" : \"$SERVER_NAME\","
+JSON_DATA+='"description" : "NKN node Installing","images" : [ "linode/ubuntu16.04lts","linode/ubuntu18.04","linode/ubuntu19.10","linode/ubuntu20.04" ],'
+JSON_DATA+='"is_public" : false,"rev_note" : "Set up NKN node","script" : "#!/bin/bash\ngit clone https://github.com/postarc/nkn.git\nsudo bash nkn/installmc.sh\nsudo systemctl restart nkn.service\n" }'
+echo -e "${MAG}Script Request:${NC}\n" > $LOG_FILE
+echo $JSON_DATA | jq '.' >> $LOG_FILE
+STACK_ID=$(curl -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" \
+  -X POST -d "${JSON_DATA}"  https://api.linode.com/v4/linode/stackscripts | jq '.id | tonumber')
+echo -e "${BLUE}Script ID:$STACK_ID${NC}" >> $LOG_FILE
+if [ -z $STACK_ID ] || [ $STACK_ID == "null" ]; then echo -e "${RED}Stack script creating error, result:$STACK_ID${NC}"; exit; fi
 
 COUNTER=0
 while [ $COUNTER -lt $NODE_COUNT ]; do
     SERVER_LABEL=$SERVER_NAME$(printf "%0*d\n" 3 $(($STARTING_COUNTER+$COUNTER)))
+    echo -e "${CYAN}Instance $SERVER_LABEL creating...${NC}"
     JSON_DATA="{ \"backups_enabled\": false, \"swap_size\": 1025, \"image\": \"linode/ubuntu18.04\", \"root_pass\": \"$PASSWORD\", \"stackscript_id\": $STACK_ID, \
           \"booted\": true, \"label\": \"$SERVER_LABEL\", \"type\": \"g6-nanode-1\", \"region\": \"$SERVER_REGION\", \"group\": \"Linode-Group\" }"
-
-    #echo $JSON_DATA | jq '.'
-    echo -e "${CYAN}Instances $SERVER_LABEL creating...${NC}"
-    curl -H "Content-Type: application/json"  -H "Authorization: Bearer $TOKEN" -X POST -d "${JSON_DATA}" \
-        https://api.linode.com/v4/linode/instances | jq -c '.data[].ipv4' | tr -d \"[] >> serverip.list
-	(( COUNTER++ ))
+    echo -e "${MAG}Server Request:\n${NC}" >> $LOG_FILE
+    echo $JSON_DATA | jq '.' >> $LOG_FILE
+    SERVER_IP=$(curl -H "Content-Type: application/json"  -H "Authorization: Bearer $TOKEN" -X POST -d "${JSON_DATA}" \
+        https://api.linode.com/v4/linode/instances)
+    #echo -e -n "${BLUE}Server IP:${NC}"
+    #echo $SERVER_IP | jq '.data[].ipv4' | tr -d \"[]
+    echo $SERVER_IP | jq '.' >> $LOG_FILE
+    if [ -z $SERVER_IP ] || [ $SERVER_IP == "null" ]; then echo  -e "${RED}Server creating error, result:$STACK_ID${NC}:"; exit
+    #else echo $SERVER_IP >> $SERVER_IP_FILE
+    fi
+    (( COUNTER++ ))
 done
 
+if [[ $STACK_ID =~ ^[0-9]+$ ]] ; then
+    echo -e "${CYAN}Stack script removing...${NC}"
+    echo -e "${MAG}Script Remove Request:\n${NC}" >> $LOG_FILE
+    curl -H "Authorization: Bearer $TOKEN" -X DELETE https://api.linode.com/v4/linode/stackscripts/$STACK_ID | jq '.' >> $LOG_FILE
+fi
+echo
+echo -e "${CYAN}Servers IP list:${NC}"
+curl -H "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances | jq -c '.data[].ipv4' | tr -d \"[]
+echo
+echo -e "${GREEN}All done!!! Servers created: $COUNTER ${NC}"
